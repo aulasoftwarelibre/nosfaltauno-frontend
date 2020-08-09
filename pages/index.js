89bb10af2e36bb6aa4c486602fcfad64c2da110a
Sergio Gómez <sergio@uco.es>
2020-08-09 23:27:20 +0200

    Update index page with bumbag components

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,46 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import {
+  Button,
+  Heading,
+  Hide,
+  Image,
+  PageContent,
+  PageWithHeader,
+  Paragraph,
+  TopNav,
+} from 'bumbag';
+import React from 'react';
 
-export default function Home() {
+export default function Index() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    <>
+      <PageWithHeader
+        sticky
+        header={
+          <TopNav backgroundColor="#2d3748" color="#ffffff">
+            <TopNav.Section>
+              <TopNav.Item href="/" fontWeight="semibold">
+                <Image src="/dos-lineas-tinta-blanca.svg" height="44px" />
+              </TopNav.Item>
+              <Hide below="desktop">
+                <TopNav.Item href="#">Option 1</TopNav.Item>
+                <TopNav.Item href="#">Option 2</TopNav.Item>
+              </Hide>
+            </TopNav.Section>
+            <TopNav.Section marginRight="major-2">
+              <TopNav.Item>
+                <Button palette="white">Login</Button>
+              </TopNav.Item>
+            </TopNav.Section>
+          </TopNav>
+        }
+        border="none"
+      >
+        <PageContent>
+          <Heading>Bumbag's Basic Next.js Example</Heading>
+          <Paragraph>Welcome to your new Bumbag Next.js site.</Paragraph>
+          <Paragraph>Now go build something great with your Bumbag!</Paragraph>
+        </PageContent>
+      </PageWithHeader>
+    </>
+  );
 }
