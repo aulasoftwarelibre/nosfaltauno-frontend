67c01594515e813cee03b44b15259650388b2239
Sergio Gómez <sergio@uco.es>
2020-09-17 23:37:56 +0200

    Add next-auth authentication plugin (#6)
    * Add dependencies
    
    * Add nextauth
    
    * Configure route to backend
    
    * Added login example page
    
    * Upgrade dependencies
    
    Co-authored-by: Javier Cañete Puentenueva <i62capuj@uco.es>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { signIn, signOut, useSession } from "next-auth/client";
+import { Avatar } from "@material-ui/core";
 
 export default function Index() {
+  const [session, loading] = useSession();
+
   return (
-    <div>
-      <h1> Hello World</h1>
-    </div>
+    <>
+      {!session && (
+        <>
+          Not signed in <br />
+          <button onClick={signIn}>Sign in</button>
+        </>
+      )}
+      {session && (
+        <>
+          <Avatar alt="Remy Sharp" src={session.user.image} />
+          Signed in as {session.user.email} <br />
+          <button onClick={signOut}>Sign out</button>
+        </>
+      )}
+    </>
   );
 }
